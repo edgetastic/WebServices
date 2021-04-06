61a4cf1b18a37ba3803f0005a41bf644f40a9639
Maxime Beaulieu <maxime_beaulieu@live.ca>
2021-04-06 16:03:49 -0400

    Merge confict

--- the-network/backend/thenetworkapi/queries.sql
+++ the-network/backend/thenetworkapi/queries.sql
@@ -1,32 +1,18 @@
-<<<<<<< HEAD
-CREATE DATABASE `thenetworkdb` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */;
-USE `thenetworkdb`
-
-CREATE TABLE `users` (
-  `userId` int(11) NOT NULL AUTO_INCREMENT,
-=======
 CREATE DATABASE `thenetworkdb` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
 
 CREATE TABLE `users` (
   `userId` int NOT NULL AUTO_INCREMENT,
->>>>>>> d1f487c90df86664d3e6e4dad263695acef27f09
   `email` varchar(200) NOT NULL,
   `firstName` varchar(200) NOT NULL,
   `lastName` varchar(200) NOT NULL,
   `dateOfBirth` datetime NOT NULL,
-<<<<<<< HEAD
-  `profilePicture` blob,
-=======
   `profilePicture` varchar(200) DEFAULT NULL,
->>>>>>> d1f487c90df86664d3e6e4dad263695acef27f09
   `creationDate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `password` varchar(250) NOT NULL,
   PRIMARY KEY (`userId`),
   UNIQUE KEY `email` (`email`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-<<<<<<< HEAD
-=======
 
 CREATE TABLE `posts` (
   `postId` int NOT NULL AUTO_INCREMENT,
@@ -52,4 +38,3 @@ CREATE TABLE `comments` (
   CONSTRAINT `fk_post_id` FOREIGN KEY (`postId`) REFERENCES `posts` (`postId`),
   CONSTRAINT `fk_user_id` FOREIGN KEY (`userId`) REFERENCES `users` (`userId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
->>>>>>> d1f487c90df86664d3e6e4dad263695acef27f09
